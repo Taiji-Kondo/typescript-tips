cfe0e8afa5b724be306311d28d5401dedcc0976d
taiji <k.taiji1108@gmail.com>
2023-04-19 22:51:41 +0900

    feat: Edit Mapped Types Demo

--- src/pages/mappedTypes/index.tsx
+++ src/pages/mappedTypes/index.tsx
@@ -3,3 +3,18 @@ export default function MappedTypesPage() {
     <h1>Mapped Types Demo</h1>
   )
 }
+
+type ProductType = {
+  name: string,
+  price: number
+  createdAt: Date,
+}
+
+type CustomErrorType = { code: number, message: string }
+
+// Modelの型をもとにResponseの型情報を生成するMapped Typesを定義
+type ResponseType<T> = {
+  [Property in keyof T]: T[Property] | CustomErrorType;
+}
+// Product ModelのResponse型を生成
+type ProductResponseType = ResponseType<ProductType>
