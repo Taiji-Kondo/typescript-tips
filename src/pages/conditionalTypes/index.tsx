24a417c40018151a70ee1eccb552abb59808aeab
taiji <k.taiji1108@gmail.com>
2023-04-19 19:57:21 +0900

    feat: Edit Infer Demo

--- src/pages/conditionalTypes/index.tsx
+++ src/pages/conditionalTypes/index.tsx
@@ -1,3 +1,12 @@
+// ReturnTypeのデモ
+type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+const add = (a: number, b: number) => a + b
+
+type AddReturnType = ReturnType<typeof add> // number
+
+// 1. `ReturnType`の`Generics`として`(a: number, b: number) => number`型の関数を受け取る
+// 2. 受け取った関数が`ConditionalType`の条件にマッチするため、戻り値である`number`が`R`にキャプチャされ返却
+
 export default function InferPage() {
   return (
     <h1>Infer Demo</h1>
